2a6472249d34b9a359694d0d94840c5f34287cb3
hb253 <horia@balog.com>
2026-04-17 18:06:40 -0400

    Update generate_samples.py

--- generate_samples.py
+++ generate_samples.py
@@ -1,6 +1,8 @@
 import random
 import csv
 
+// This script generates data samples to use as seed when one doesn't have an initial set of data to start with
+
 def generate_samples(n=100, output_file="samples.csv"):
     values = [random.random() for _ in range(n)]
 
